2a51da8e3319562e4b4ece054cb0ba16bf8990be
sun <minhua.sun@aixplorerbio.com>
2022-04-14 14:07:58 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ sudo bash create_slurm_user_without_sacct.sh
 
 
 sudo service munge start
-sudo slurmd -N $SLURM_NODENAME
+sudo slurmd -N $SLURM_NODENAME -f /public/slurm/slurm.conf
 
 tail -f /dev/null
